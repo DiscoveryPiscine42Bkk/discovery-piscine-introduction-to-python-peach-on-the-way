0ad58e7122aa97251ea0cc67eb5894311cf3764d
peach-on-the-way <peachinhome@gmail.com>
2025-06-23 18:56:30 +0700

    Cellule5.9

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# • Create a program called scan_it.py.
+# • This program should take two parameters.
+# • The first parameter is a keyword to search for in a string.
+# • The second parameter is the string to be searched.
+# • This program should be executable.
+# • When executed, the program should display the number of times the keyword ap-
+# pears in the string.
+# • If the number of parameters is different from 2 or if the first string does not appear
+# in the second, it should display none followed by a newline.
+# ?> ./scan_it.py | cat -e
+# none$
+# ?> ./scan_it.py "the" | cat -e
+# none$
+# ?> ./scan_it.py "the" "the quick brown fox jumps over the lazy dog" | cat -e
+# 2$
+# ?>
+
+import sys
+import re
+
+if len(sys.argv) - 1 != 2:
+    print("none")
+    exit()
+
+keyword = sys.argv[1]
+string = sys.argv[2]
+result = re.findall(keyword, string)
+print(len(result))
