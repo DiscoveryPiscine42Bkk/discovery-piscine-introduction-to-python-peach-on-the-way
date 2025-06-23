d323cb9c5b5b5258c76fb9c618942b3e832b9922
peach-on-the-way <peachinhome@gmail.com>
2025-06-23 18:10:18 +0700

    Cellule5.0

---
+++ cell05/ex00/create_array.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# • Create a program called create_array.py.
+# • This program should be executable.
+# • You will define an array of numbers.
+# • You will display your array on the screen:
+# ?> ./create_array.py | cat -e
+# [2, 8, 9, 48, 8, 22, -12, 2]$
+# ?>
+
+print([2, 8, 9, 48, 8, 22, -12, 2])
