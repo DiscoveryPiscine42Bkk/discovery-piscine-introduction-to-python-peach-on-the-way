88ac0bb5fec5a713e167b5034f723658ddf30b99
peach-on-the-way <peachinhome@gmail.com>
2025-06-23 18:09:03 +0700

    Cellule4.5

---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# • Create a program called up_low.py.
+# • This program should be executable.
+# • Your script will ask the user for a string.
+# • You should then display the string with uppercase letters changed to lowercase and
+# vice versa.
+# ?> ./up_low.py
+# Hello World
+# hELLO wORLD
+# ?>
+# ?> ./up_low.py
+# aaaaAAAA
+# AAAAaaaa
+# ?>
+# ?> ./up_low.py
+# hello 42
+# HELLO 42
+# ?>
+
+print(input().swapcase())
