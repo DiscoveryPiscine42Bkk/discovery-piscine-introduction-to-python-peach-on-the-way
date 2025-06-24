0ca4bb0fb93aec360d8dfc33c6f3bcb123529e86
peach-on-the-way <peachinhome@gmail.com>
2025-06-24 13:59:35 +0700

    Cellule7.1

---
+++ cell07/ex01/family_affairs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# • Create a script called family_affairs.py.
+# • It will contain a method called find_the_redheads.
+# • This method will take a dictionary as a parameter, representing family members
+# with their first name as the key and their hair color as the value.
+# • This method will use the filter function to gather the first names of the red-haired
+# individuals into a new list, which it will return.
+# • For example, the following script:
+# # your method definition here
+# dupont_family = {
+# "florian": "red",
+# "marie": "blond",
+# "virginie": "brunette",
+# "david": "red",
+# "franck": "red"
+# }
+# print(find_the_redheads(dupont_family))
+# Will have the following output:
+# ?> ./family_affairs.py
+# ['florian', 'david', 'franck']
+# ?>
+
+def find_the_redheads(members):
+    return [name for name, color in members.items() if color == "red"]
+
+dupont_family = {
+    "florian": "red",
+    "marie": "blond",
+    "virginie": "brunette",
+    "david": "red",
+    "franck": "red"
+}
+
+print(find_the_redheads(dupont_family))
