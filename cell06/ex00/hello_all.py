beb4a61bac059c060bc3e10297013ec577f8b9a6
peach-on-the-way <peachinhome@gmail.com>
2025-06-24 13:04:42 +0700

    Cellule6.0

---
+++ cell06/ex00/hello_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#  Create a program hello_all.py.
+# • This program must be executable.
+# • This program will contain a method called "hello". This method will display "Hello,
+# everyone!".
+# • After defining your method, you will test it by calling it in your program. Like in
+# the example below, except that we have hidden the method definition.
+# ?> cat hello_all.py
+# # Your method definition
+# hello()
+# ?> ./hello_all.py
+# Hello, everyone!
+# ?>
+
+def hello():
+    print("Hello, everyone!")
+
+hello()
